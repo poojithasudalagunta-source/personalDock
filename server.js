d638726e470ba9ecc93b9feb6fcf30ed5c77acb5
Eos Poojitha Sudalagunta <poojithasudalagunta@gmail.com>
2026-04-04 00:42:16 +0530

    placing

--- server.js
+++ server.js
@@ -348,10 +348,11 @@ app.post("/knowledge/edit", async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
+app.get("/", (req, res) => {
+  res.send("Server is running ✅");
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
-app.get("/", (req, res) => {
-  res.send("Server is running ✅");
-});
